7a9fa05551fe03eac8472dbabf3a5c02423925e6
Betsy Rowley <BRowley8@gmail.com>
2017-06-25 12:41:47 -0500

    Adds Controllers

--- client/scripts/myApp.js
+++ client/scripts/myApp.js
@@ -7,17 +7,12 @@ angular
 function config($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('');
   $routeProvider
-  .when('/caseworker-appointments-all', {
-    templateUrl: '/views/templates/caseworker-appointments-all.html',
-    controller: 'CaseworkerAppointmentsAllController',
+  .when('/about', {
+    templateUrl: '/views/templates/about.html',
+    controller: 'AboutController',
     controllerAs: 'vm',
-    resolve: { // get user from factory
-      getuser: ['UserService', function(UserService) {
-        return UserService.getUser();
-      }]
-    }
   })
   .otherwise({
-    redirectTo: 'login'
+    redirectTo: 'home'
   });
 }
